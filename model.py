b653147ad68d532bd310c6f12e3fca2c1af712b0
linkdom <donglinkang2021@163.com>
2024-02-03 12:11:15 +0800

    refer resnet18 and get acc 0.71

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from typing import List, Tuple, Optional
 
 class BaseModel(nn.Module):
     def __init__(self):
@@ -21,37 +22,90 @@ class BaseModel(nn.Module):
 
     def forward(self, x):
         raise NotImplementedError
+    
+def conv3x3(in_planes: int, out_planes: int, stride: int = 1) -> nn.Conv2d:
+    """3x3 convolution with padding"""
+    return nn.Conv2d(
+        in_planes,
+        out_planes,
+        kernel_size=3,
+        stride=stride,
+        padding=1,
+        bias=False
+    )
+
+
+def conv1x1(in_planes: int, out_planes: int, stride: int = 1) -> nn.Conv2d:
+    """1x1 convolution"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+
+
+class BasicBlock(nn.Module):
+    def __init__(
+        self,
+        inplanes: int,
+        planes: int,
+        stride: int = 1,
+    ) -> None:
+        super(BasicBlock, self).__init__()
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.stride = stride
+        if stride != 1 or inplanes != planes:
+            self.downsample = nn.Sequential(
+                conv1x1(inplanes, planes, stride),
+                nn.BatchNorm2d(planes),
+            )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
 
 class CNN(BaseModel):
-    def __init__(self, in_channels: int, n_channels, n_classes: int):
+    def __init__(self, in_channels: int, n_channels: List[int], n_classes: int):
         super().__init__()
-        self.b1 = nn.Sequential(
+        self.downsample = nn.Sequential(
             nn.Conv2d(
                 in_channels, 
-                out_channels = 64, 
+                n_channels, 
                 kernel_size = 7, 
                 stride = 2, 
                 padding = 3           
             ),
-            nn.BatchNorm2d(64), 
+            nn.BatchNorm2d(n_channels), 
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         )
-        self.res1 = nn.Sequential(
-            nn.Conv2d(64, 64, 3, 1, 1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.Conv2d(64, 64, 3, 1, 1),
-            nn.BatchNorm2d(64)
-        )
+        self.res1 = BasicBlock(n_channels, 2 * n_channels, stride=2)
+        self.res2 = BasicBlock(2 * n_channels, 4 * n_channels, stride=2)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.flatten = nn.Flatten()
+        self.fc = nn.Linear(4 * n_channels, n_classes)
         self.apply(self._init_weights)
         print(f"number of parameters: {self.get_num_params()/1e6:.6f} M ")
 
     def forward(self, x):
-        x = self.pool(F.relu(self.bn1(self.conv1(x))))
-        x = self.pool(F.relu(self.bn2(self.conv2(x))))
-        x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = self.downsample(x)  # B, 3, 32, 32  -> B, 64, 16, 16
+        x = self.res1(x)        # B, 64, 16, 16 -> B, 128, 8, 8
+        x = self.res2(x)        # B, 128, 8, 8  -> B, 256, 4, 4
+        x = self.avgpool(x)     # B, 256, 4, 4  -> B, 256, 1, 1
+        x = self.flatten(x)     # B, 256, 1, 1  -> B, 256
+        x = self.fc(x)          # B, 256        -> B, 10
         return x
--- train.py
+++ train.py
@@ -9,32 +9,38 @@ from model import CNN
 import numpy as np
 
 # config
-in_channels=3
-n_classes=10
-batch_size = 64
+in_channels = 3
+n_channels = 64
+n_classes = 10
+batch_size = 128
 learning_rate = 1e-3
 num_epochs = 20
 eval_interval = 10
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 # ---------------------
+
+torch.manual_seed(2024)
+np.random.seed(2024)
 
 # 定义数据转换
 transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
 
 # # 使用MNIST类加载数据集
-# train_dataset = MNIST(root='./data', train=True, transform=transform, download=True)
-# test_dataset = MNIST(root='./data', train=False, transform=transform, download=True)
-train_dataset = CIFAR10(root='./data', train=True, transform=transform, download=True)
-test_dataset = CIFAR10(root='./data', train=False, transform=transform, download=True)
+# train_dataset = MNIST(root='./data', train=True, transform=transform)
+# test_dataset = MNIST(root='./data', train=False, transform=transform)
+train_dataset = CIFAR10(root='./data', train=True, transform=transform)
+test_dataset = CIFAR10(root='./data', train=False, transform=transform)
 train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
 
 
-model = CNN(in_channels, n_classes)
+model = CNN(in_channels, n_channels, n_classes)
+model.to(device)
 
 criterion = nn.CrossEntropyLoss()
 
 # 优化器
-optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+optimizer = optim.AdamW(model.parameters(), lr=learning_rate)
 
 
 @torch.no_grad()
@@ -46,6 +52,7 @@ def estimate():
         num_samples = 0
         num_correct = 0
         for x, y in loader:
+            x, y = x.to(device), y.to(device)
             y_pred = model(x)
             loss = criterion(y_pred, y)
             losses.append(loss.item())
@@ -60,8 +67,8 @@ def estimate():
 n_batches = len(train_loader)
 for epoch in range(num_epochs):
     for i, (x, y) in enumerate(train_loader):
-        iter = epoch * n_batches + i
 
+        iter = epoch * n_batches + i
         if iter % eval_interval == 0 or iter == num_epochs * n_batches - 1:
             metrics = estimate()
             print(f"step {iter}:", end=' ')
@@ -69,8 +76,21 @@ for epoch in range(num_epochs):
                 print(f"{k}: {v:.4f}", end=' ')
             print()
 
+        x, y = x.to(device), y.to(device)
         y_pred = model(x)
         loss = criterion(y_pred, y)
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+
+"""output:
+(GPT) root@test:~/VisionAttention# python train.py
+number of parameters: 1.161354 M 
+...
+step 1080: train: 0.5854 train_acc: 0.7960 test: 0.8246 test_acc: 0.7176 
+step 1090: train: 0.6275 train_acc: 0.7778 test: 0.8842 test_acc: 0.6899 
+step 1100: train: 0.6384 train_acc: 0.7764 test: 0.8978 test_acc: 0.6944 
+step 1110: train: 0.5823 train_acc: 0.7965 test: 0.8255 test_acc: 0.7105 
+step 1120: train: 0.6351 train_acc: 0.7738 test: 0.8713 test_acc: 0.6997 
+step 1130: train: 0.5973 train_acc: 0.7902 test: 0.8415 test_acc: 0.7095 
+"""
