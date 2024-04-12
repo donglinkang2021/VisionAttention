5361e2e663ea75a4de5b16c6d96ee6e6e8dec1b4
Linkdom <donglinkang2021@163.com>
2024-04-12 15:18:37 +0800

    modify datasets.py

--- datasets.py
+++ datasets.py
@@ -4,13 +4,25 @@ from torch.utils.data import DataLoader
 
 def get_loader(datasets: str, batch_size: int):
     if datasets == 'mnist':
-        transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
+        transform = transforms.Compose([
+            transforms.ToTensor(), 
+            transforms.Normalize((0.5,), (0.5,))
+        ])
         train_dataset = MNIST(root='./data', train=True, transform=transform)
         test_dataset = MNIST(root='./data', train=False, transform=transform)
     elif datasets == 'cifar10':
-        transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))])
-        train_dataset = CIFAR10(root='./data', train=True, transform=transform)
-        test_dataset = CIFAR10(root='./data', train=False, transform=transform)
+        transform_train = transforms.Compose([
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomVerticalFlip(),
+            transforms.ToTensor(), 
+            transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+        ])
+        transform_test = transforms.Compose([
+            transforms.ToTensor(), 
+            transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+        ])
+        train_dataset = CIFAR10(root='./data', train=True, transform=transform_train)
+        test_dataset = CIFAR10(root='./data', train=False, transform=transform_test)
     else:
         raise ValueError(f"datasets should be 'mnist' or 'cifar10', but got {datasets}.")
     train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
