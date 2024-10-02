4425e3f5d617d21a39572d576f69050e97321af0
Linkdom <donglinkang2021@163.com>
2024-10-02 21:16:50 +0800

    make wandb offline

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 python main.py --multirun \
-    module.learning_rate=3e-4,3e-3,3e-2 \
+    module.learning_rate=3e-4,3e-2 \
     optimizer=adam,sgd \
     scheduler=default,steplr \
-    module.backbone=resnet18,resnet34 \
+    module.backbone=resnet18,resnet50 \
